54a8eb9cf20a600060d96cec31bc77bfab9888e5
Mercedes Egido <meegido@gmail.com>
2015-03-24 12:43:59 +0100

    second algorithm implemented

--- leap-year.js
+++ leap-year.js
@@ -1,4 +1,4 @@
-function leap(year){
+function leapLogic(year){
 	if ((year % 4 === 0) && (year % 100 === 0 || year % 400 === 0)){
 		return true
 	}
@@ -7,8 +7,19 @@ function leap(year){
 	}
 }
 
+function leap(year){
+	if (((year % 4 === 0) && (year % 100 !== 0)) || (year % 400 === 0)){
+		return true
+	}
+	return false
+}
 
-console.log(leap(2000));
-console.log(leap(2003));
-console.log(leap(2015));
-console.log(leap(1999));
+
+console.log(leapLogic(2000) == true);
+console.log(leapLogic(2003) == false);
+console.log(leapLogic(2015) == false);
+console.log(leapLogic(1999) == false);
+
+console.log(leap(2000) == true);
+console.log(leap(2003) == false);
+console.log(leap(2015) == false);
