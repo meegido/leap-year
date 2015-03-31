94519dcf2fcad8068a006d168daca290f079a685
Mercedes Egido <meegido@gmail.com>
2015-03-31 11:47:22 +0200

    refactor with ifs

--- leap-year.js
+++ leap-year.js
@@ -1,38 +1,29 @@
-function divisibleByFour(number){
-	return number % 4 === 0; 
-}
-
-function divisibleByHundred(number){
-	return number % 100 === 0;
-}
-
-function divisibleByFourHundred(number){
-	return number % 400 === 0;
-}
-
-
 function leapLogic(year){
-	if ((divisibleByFour(year)) && (divisibleByHundred(year) || divisibleByFourHundred(year))){
-		return true
-	}
-	else {
-		return false
-	}
+	var divisibleByFour = year % 4 === 0;
+	var divisibleByHundred = year % 100 === 0;
+	var divisibleByFourHundred = year % 400 === 0;
+
+	return divisibleByFour && divisibleByHundred || divisibleByFourHundred
+
+
+
 }
 
 function leap(year){
-	if (((divisibleByFour(year)) && (!divisibleByHundred(year))) || (divisibleByFourHundred(year))){
-		return true
-	}
-	return false
+	var divisibleByFour = year % 4 === 0;
+	var divisibleByHundred = year % 100 === 0;
+	var divisibleByFourHundred = year % 400 === 0;
+
+	return ((divisibleByFour) && (!divisibleByHundred)) || divisibleByFourHundred
 }
 
 
-console.log(leapLogic(2000) == true);
-console.log(leapLogic(2003) == false);
-console.log(leapLogic(2015) == false);
-console.log(leapLogic(1999) == false);
+console.log(leapLogic(2000) === true);
+console.log(leapLogic(2003) === false);
+console.log(leapLogic(2015) === false);
+console.log(leapLogic(1999) === false);
 
-console.log(leap(2000) == true);
-console.log(leap(2003) == false);
-console.log(leap(2015) == false);
+console.log(leap(2000) === true);
+console.log(leap(2003) === false);
+console.log(leap(2015) === false);
+console.log(leap(1999) === false);
